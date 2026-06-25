4179f2f448b3f0ae2402e4f3421a7497c34ce45c
LAKSHYA SHARMA <lakshyasharma8005@gmail.com>
2026-06-25 15:43:24 +0530

    ADDED SPEED INSIGHTS

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,7 @@ export const metadata: Metadata = {
   description: "You are on a better path.",
 };
 import { Analytics } from "@vercel/analytics/next"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 export default function RootLayout({
   children,
 }: Readonly<{
